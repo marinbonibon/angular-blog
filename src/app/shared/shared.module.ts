4d020405a6c8c03763e980fce179a0b578a3ee44
Maryna Petrenko <marinbonibon@gmail.com>
2020-03-09 22:11:34 +0300

    add quill plugin, creating of post

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import {QuillModule} from 'ngx-quill';
 
 
 
 
 @NgModule({
   imports: [
-   HttpClientModule
+   HttpClientModule,
+    QuillModule.forRoot()
   ],
   exports: [
-    HttpClientModule
+    HttpClientModule,
+    QuillModule
   ]
 })
 export class SharedModule { }
