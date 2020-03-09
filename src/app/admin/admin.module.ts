e26afc001fcfe6c3abb579d2dc0435ccf87abc7b
Maryna Petrenko <marinbonibon@gmail.com>
2020-03-09 20:07:19 +0300

    implement login logic

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -7,6 +7,8 @@ import { DashboardPageComponent } from './dashboard-page/dashboard-page.componen
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AuthService} from './shared/services/auth.service';
+import {SharedModule} from '../shared/shared.module';
 
 
 
@@ -16,6 +18,7 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    SharedModule,
     RouterModule.forChild([
       {path: '', component: AdminLayoutComponent, children: [
           {path: '', redirectTo: '/admin/login', pathMatch: 'full'},
@@ -27,6 +30,7 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
     ])
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthService]
 })
 export class AdminModule { }
