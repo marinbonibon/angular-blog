ac4edf64b3084264def138b797e11bfc4c86d023
Maryna Petrenko <marinbonibon@gmail.com>
2020-03-10 15:36:31 +0300

    add filter pipe, align code, fix mistake in text

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  apiKey: 'AIzaSyAj-GYXZyLYzfUBpeQgiDLbOylUhv6RSSE',
+  production: true,
+  fbDbUrl: 'https://angular-blog-a8667.firebaseio.com/'
 };
