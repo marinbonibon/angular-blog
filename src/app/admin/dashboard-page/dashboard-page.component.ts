216c2efbe253ab6b504fd75d041e4cc41db19a2e
Maryna Petrenko <marinbonibon@gmail.com>
2020-03-10 15:47:17 +0300

    add logic of post's removing

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -11,6 +11,7 @@ import {Subscription} from 'rxjs';
 export class DashboardPageComponent implements OnInit, OnDestroy {
   posts: Post[] = [];
   pSub: Subscription;
+  dSub: Subscription;
   searchStr = '';
 
   constructor(private postsService: PostsService) { }
@@ -25,10 +26,15 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
     if (this.pSub) {
       this.pSub.unsubscribe();
     }
+    if (this.dSub) {
+      this.dSub.unsubscribe();
+    }
   }
 
   remove(id: string) {
-    console.log('this.posts', this.posts);
+    this.postsService.remove(id).subscribe(() =>  {
+      this.posts = this.posts.filter(post => post.id !== id);
+    });
 
   }
 }
