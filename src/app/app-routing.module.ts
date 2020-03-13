658636a90b1bf2eee72980840f7cc62ba141308e
Maryna Petrenko <marinbonibon@gmail.com>
2020-03-13 17:24:21 +0300

    migrate to angular 9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
     ]
   },
   {
-    path: 'admin', loadChildren: './admin/admin.module#AdminModule'
+    path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
   }
 ];
 
